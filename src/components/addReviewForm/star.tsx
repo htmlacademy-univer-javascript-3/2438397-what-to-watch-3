1f7dc0d2f2ae1f940dc37f35edb275ae8fbf46df
Ivan Moskovchenko <imosk72@yandex-team.ru>
2023-10-16 00:57:03 +0300

    fix quotes

--- src/components/addReviewForm/star.tsx
+++ src/components/addReviewForm/star.tsx
@@ -1,12 +1,12 @@
-import React, {ReactElement} from 'react';
+import {Fragment, ReactElement} from 'react';
 
 import {StarProps} from '../../propsTypes/componentsPropsTypes';
 
 export function Star({value, onClick}: StarProps): ReactElement {
   return (
-    <React.Fragment>
+    <Fragment>
       <input className="rating__input" id={`star-${value}`} type="radio" name="rating" value={value} onClick={onClick} />
       <label className="rating__label" htmlFor={`star-${value}`}>Rating {value}</label>
-    </React.Fragment>
+    </Fragment>
   );
 }
--- src/pages/myListPage/myListPage.tsx
+++ src/pages/myListPage/myListPage.tsx
@@ -8,10 +8,10 @@ import {MyFilmsPageProps} from '../../propsTypes/pagesProsTypes';
 
 export function MyListPage({filmsCardList}: MyFilmsPageProps): ReactElement {
   return (
-    <div className='user-page'>
-      <header className='page-header user-page__head'>
+    <div className="user-page">
+      <header className="page-header user-page__head">
         <Logo />
-        <h1 className='page-title user-page__title'>My list <span className='user-page__film-count'>{filmsCardList.length}</span></h1>
+        <h1 className="page-title user-page__title">My list <span className="user-page__film-count">{filmsCardList.length}</span></h1>
         <UserBlock />
       </header>
 
