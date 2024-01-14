82894a3d65d201027b82f866b986ee0043cb59f8
Ivan Moskovchenko <imosk72@yandex-team.ru>
2024-01-14 19:48:53 +0300

    fix consts in time converters

--- src/helpers/time-converter.ts
+++ src/helpers/time-converter.ts
@@ -1,19 +1,25 @@
+const SECOND_DURATION = 1;
+const MINUTE_DURATION = SECOND_DURATION * 60;
+const HOUR_DURATION = MINUTE_DURATION * 60;
+
+const MAX_NUMBER_TO_ADD_ZERO = 9;
+
 function getNumberWithLeadZeroes(number: number): string {
-  if (Math.round(number) < 10) {
+  if (Math.round(number) <= MAX_NUMBER_TO_ADD_ZERO) {
     return `0${Math.round(number)}`;
   }
   return `${Math.round(number)}`;
 }
 
 export function convertTimeToPlayerFormat(time: number): string {
-  if (time < 60 * 60) {
+  if (time < HOUR_DURATION) {
     return `-${getNumberWithLeadZeroes(
-      Math.floor(time / 60),
-    )}:${getNumberWithLeadZeroes(time % 60)}`;
+      Math.floor(time / MINUTE_DURATION),
+    )}:${getNumberWithLeadZeroes(time % MINUTE_DURATION)}`;
   }
   return `-${getNumberWithLeadZeroes(
-    Math.floor(time / 3600),
+    Math.floor(time / HOUR_DURATION),
   )}:${getNumberWithLeadZeroes(
-    Math.floor((time % 3600) / 60),
-  )}:${getNumberWithLeadZeroes(time % 60)}`;
+    Math.floor((time % HOUR_DURATION) / MINUTE_DURATION),
+  )}:${getNumberWithLeadZeroes(time % MINUTE_DURATION)}`;
 }
