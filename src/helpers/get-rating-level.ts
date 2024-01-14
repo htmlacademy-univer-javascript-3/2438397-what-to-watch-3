32c5773166ea689a548a0bc57ba9e8de90e29d13
Ivan Moskovchenko <imosk72@yandex-team.ru>
2024-01-14 19:29:40 +0300

    fix rating nums

--- src/helpers/get-rating-level.ts
+++ src/helpers/get-rating-level.ts
@@ -1,16 +1,16 @@
-const MAX_BAD_RATING = 2;
-const MAX_NORMAL_RATING = 4;
-const MAX_GOOD_RATING = 7;
-const MAX_VERY_GOOD_RATING = 9;
+const MAX_BAD_RATING = 3;
+const MAX_NORMAL_RATING = 5;
+const MAX_GOOD_RATING = 8;
+const MAX_VERY_GOOD_RATING = 10;
 
 export function getRatingLevel(rating: number): string {
-  if (rating <= MAX_BAD_RATING) {
+  if (rating < MAX_BAD_RATING) {
     return 'Bad';
-  } else if (rating <= MAX_NORMAL_RATING) {
+  } else if (rating < MAX_NORMAL_RATING) {
     return 'Normal';
-  } else if (rating <= MAX_GOOD_RATING) {
+  } else if (rating < MAX_GOOD_RATING) {
     return 'Good';
-  } else if (rating <= MAX_VERY_GOOD_RATING) {
+  } else if (rating < MAX_VERY_GOOD_RATING) {
     return 'Very good';
   } else {
     return 'Awesome';
